1b77ad86752d84b29990a1d6e37fc24ab86841a0
Marius Jakobi <marius.jakobi@googlemail.com>
2021-10-30 16:36:09 +0200

    feat: users can now be registered

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { RegisterUserDto } from 'src/auth/dto/register-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
@@ -10,15 +11,42 @@ export class UsersService {
     private usersRepository: Repository<User>
   ) { }
 
+  /**
+   * Return a list of all users
+   * @returns List of users
+   */
   async findAll(): Promise<User[]> {
     return this.usersRepository.find();
   }
 
+  /**
+   * Return a single user by ID
+   * @param id User ID
+   * @returns A single user by ID
+   */
   async findById(id: number): Promise<User> {
     return this.usersRepository.findOne(id);
   }
 
+  /**
+   * Return a single user by username
+   * @param username Username
+   * @returns A user by username
+   */
   async findByUsername(username: string): Promise<User> {
     return this.usersRepository.findOne({ username })
+  }
+
+  /**
+   * Create one user and return it
+   * @param registerUserDto A RegisterUserDTO
+   * @returns A newly created User
+   */
+  async createOne(registerUserDto: RegisterUserDto): Promise<User> {
+    const user = new User();
+    user.username = registerUserDto.username;
+    user.password = registerUserDto.password;
+    
+    return this.usersRepository.save(user);
   }
 }
